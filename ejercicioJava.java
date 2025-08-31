02e90ac036ca0beb7b64a516a692c6d91b1b3304
Renzo Caycho <renzocaycho@outlook.es>
2025-08-31 17:29:04 -0600

    Ejercicios Python y Java 1
    Parte 1

---
+++ ejercicioJava.java
@@ -0,0 +1,11 @@
+class ejercicioJava {
+    public static void main(String[] args) {
+        int x=10;
+        int b=10;
+        //System.out.println(x++);
+        //System.out.println(++x);
+        //System.out.println(b++ * -3 == -3 % x++);
+        //System.out.println(30 - 10 % ++b + 30 % 7 * 2);
+        System.out.println(x =  -50 * x++ * 80 % b / 4 *8);
+    }
+}
